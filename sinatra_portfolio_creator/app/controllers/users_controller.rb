25fb9555ea13072b622e70cc84d69a06aecb7ba4
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-25 01:06:13 -0600

    add login functionality

--- sinatra_portfolio_creator/app/controllers/users_controller.rb
+++ sinatra_portfolio_creator/app/controllers/users_controller.rb
@@ -9,6 +9,21 @@ class UsersController < ApplicationController
     erb :index
   end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :index
+    else
+      redirect '/login'
+    end
+  end
+
+
   # get '/users/portfolio' do
   #   erb :'users/portfolio'
   # end
