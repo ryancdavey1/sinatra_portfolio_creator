b30af16b95ae7aba762eaf22993d2adc15555e6c
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-24 23:16:52 -0600

    add user controller and create signup page

---
+++ sinatra_portfolio_creator/app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    erb :'users/portfolio'
+  end
+
+  # get '/users/portfolio' do
+  #   erb :'users/portfolio'
+  # end
+end
