fbc381b0689c47067792590f41ade38bc99580dd
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-25 01:25:47 -0600

    add helper functions

--- sinatra_portfolio_creator/app/controllers/application_controller.rb
+++ sinatra_portfolio_creator/app/controllers/application_controller.rb
@@ -13,4 +13,15 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @current_user = User.find_by_id(session[:user_id])
+    end
+
+  end
+
 end
