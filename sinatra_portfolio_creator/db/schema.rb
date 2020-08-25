a45531087531ad5f62dca4948bf387ec01290fd1
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-24 23:46:47 -0600

    add password digest to user table

--- sinatra_portfolio_creator/db/schema.rb
+++ sinatra_portfolio_creator/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_162756) do
+ActiveRecord::Schema.define(version: 2020_08_25_052118) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_162756) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
