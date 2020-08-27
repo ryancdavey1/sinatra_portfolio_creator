16e85882f5e6b5442dabb4518bb7a4c59c9d09ae
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-27 10:55:27 -0600

    prrotect edit and delete routes

--- sinatra_portfolio_creator/app/controllers/projects_controller.rb
+++ sinatra_portfolio_creator/app/controllers/projects_controller.rb
@@ -17,8 +17,12 @@ class ProjectsController < ApplicationController
 
   get '/projects/:id/edit' do
     @project = Project.find_by_id(params[:id])
-    erb :'projects/edit'
-  end
+    if (User.find_by(id: @project.user_id) == current_user)
+      erb :'projects/edit'
+    else 
+      redirect '/projects'
+    end
+    end
 
   post '/projects' do
 
@@ -48,27 +52,40 @@ class ProjectsController < ApplicationController
   end
 
   patch '/projects/:id' do
-
       @project = Project.find_by_id(params[:id])
-      
-      @project.update(
-        title: params[:project_title], 
-        description: params[:project_description],
-        javascript: params[:javascript],
-        html_and_css: params[:html_and_css],
-        sql: params[:sql],
-        python: params[:python],
-        java: params[:java],
-        ruby: params[:ruby],
-        swift: params[:swift],
-        c_sharp: params[:c_sharp],
-        c: params[:c],
-        php: params[:php],
-        sinatra: params[:sinatra],
-        start_date: params[:project_start_date],
-        end_date: params[:project_end_date]
-      )
+      if (User.find_by(id: @project.user_id) == current_user)
+        @project.update(
+          title: params[:project_title], 
+          description: params[:project_description],
+          javascript: params[:javascript],
+          html_and_css: params[:html_and_css],
+          sql: params[:sql],
+          python: params[:python],
+          java: params[:java],
+          ruby: params[:ruby],
+          swift: params[:swift],
+          c_sharp: params[:c_sharp],
+          c: params[:c],
+          php: params[:php],
+          sinatra: params[:sinatra],
+          start_date: params[:project_start_date],
+          end_date: params[:project_end_date]
+        )
+      end
       redirect '/projects'
+  end
+
+  delete '/projects/:id' do
+    @project = Project.find_by_id(params[:id])
+    #binding.pry
+    if (User.find_by(id: @project.user_id) == current_user)
+      @project.delete
+    end
+    redirect '/projects'
+  end
+
+  get '/projects/:id' do
+    
   end
 
 end
