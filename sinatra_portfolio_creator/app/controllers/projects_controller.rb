545ff8fa0620955571825ef2db9a5baf2849eac6
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-26 23:38:36 -0600

    add new project ddetails

--- sinatra_portfolio_creator/app/controllers/projects_controller.rb
+++ sinatra_portfolio_creator/app/controllers/projects_controller.rb
@@ -2,7 +2,9 @@ class ProjectsController < ApplicationController
 
   get '/projects' do
     if logged_in?
+
       @projects = Project.all
+      #binding.pry
       erb :'/projects/index'
     else
       redirect '/login'
@@ -14,6 +16,29 @@ class ProjectsController < ApplicationController
   end
 
   post '/projects' do
+    @project = Project.new(title: params[:project_title], 
+      description: params[:project_description], 
+      user_id: current_user.id,
+      javascript: params[:java],
+      html_and_css: params[:html_and_css],
+      sql: params[:sql],
+      python: params[:python],
+      java: params[:java],
+      ruby: params[:ruby],
+      swift: params[:swift],
+      c_sharp: params[:c_sharp],
+      c: params[:c],
+      php: params[:php],
+      sinatra: params[:sinatra],
+      start_date: params[:project_start_date],
+      end_date: params[:project_end_date]
+    )
+    if @project.save
+      redirect '/projects'
+    else
+      erb :‘posts/new’
+    end
+ 
   end
 
 end
--- sinatra_portfolio_creator/app/models/project.rb
+++ sinatra_portfolio_creator/app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  validates :title, :description, presence: true
 end
